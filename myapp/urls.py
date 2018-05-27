778ae2ec578f5f022a26dcb39f297419ba2c519b
sunsun <sunsun_kasaju@outlook.com>
2018-05-27 19:21:37 +0545

    dashboardupdated

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 	
 	# url(r'^login/$', views.user_login, name='login'),
 	url(r'^logout/$', views.user_logout, name='logout'),
-	# url(r'^home/', 'myapp.views.home', name='home'),
+	url(r'^home/', 'myapp.views.home', name='home'),
 	url(r'^$',views.index,name='index'),
 	url(r'^home/$',views.user_login, name='login'),
 	url(r'^Upload/',views.vehicle_upload, name='Vehicle_upload'),
